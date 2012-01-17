120a32696eb1eb475d3eee91483839c70a7f436a
Richard <x.soldier@seznam.cz>
2012-01-17 21:35:35 +0100

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,8 @@ Presentation::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+  config.action_controller.relative_url_root = "/oze"
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
